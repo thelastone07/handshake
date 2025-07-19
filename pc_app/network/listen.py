7f2bd87657b5bc2e48b9f034916eefd8561ce788
Dhananjaya Swain <dhananjayaswain99@gmail.com>
2025-07-19 17:44:08 +0530

    added cobs encoder decoder in python

--- pc_app/network/listen.py
+++ pc_app/network/listen.py
@@ -1,7 +1,15 @@
 import socket 
 from network.port_info import LOCAL_PORT, get_public_ip
 from crypto.keygen import sign_challenge
+import pyperclip
+import select
+import time
+
 NO_OF_CONNECTIONS = 5
+
+TEXT_DATA = b'x\01'
+IMAGE_DATA = b'x\02'
+
 
 def read(conn, buffer_size=1024, stop=b'\n'):
     """
@@ -19,6 +27,44 @@ def read(conn, buffer_size=1024, stop=b'\n'):
             break
     return data
 
+def encode_data(data: bytes) -> bytes:
+  encoded = bytearray()
+  j = 0
+  while j < len(data):
+    code_idx = len(encoded)
+    code_len = 1
+    encoded.append(0)
+    while j < len(data) and data[j] != 0 and code_len < 255:
+      encoded.append(data[j])
+      j = j + 1
+      code_len = code_len + 1
+    encoded[code_idx] = code_len
+    if j < len(data) and data[j] == 0:
+      j = j + 1
+  encoded.append(0)
+  return bytes(encoded)
+
+
+
+def decode_data(data:bytes) ->bytes:
+  data = data[:-1]
+  decoded = bytearray()
+  j = 0
+  while j < len(data):
+    code_len = data[j]
+  
+    j = j + 1
+    decoded += data[j:j+code_len-1]
+
+    j = j + code_len - 1
+
+    if code_len < 255 and j < len(data): 
+      decoded.append(0)
+    
+  return bytes(decoded)
+
+
+
 
 def listen():
     """
@@ -29,15 +75,16 @@ def listen():
     ip = get_public_ip()
     server.bind((f'{ip}', LOCAL_PORT))
     server.listen(NO_OF_CONNECTIONS)
+    server.setblocking(0)
 
     print(f"Listening on port {LOCAL_PORT}..."  )
+
     while True:
         conn, addr = server.accept()
         print(f"Connection established from {addr}")
-        # conn.sendall(b"b'\xb2\x8f\xfd\xf7eY\x9b?L6\x9a\x7f\xd1\xe7\xd9\x16\x89\xb9\x19\xaf\xf3\xd6\xd3\x9b\xa7\xbe\xf9\x16\xe3\x97\xe9\xdd3.a\xbaK\xbf)\xdf\xd1\xd5\x18\xac!\xaaEj\xa1\x15\x9f*\x04w\xd5\x01\xd1 \xf2\xa5~\xc1i\x08'")
-        if addr :
+        with conn:
             data = read(conn,1024)
-            print(f"Received data: {data}")
+            # print(f"Received data: {data}")
             parts = data.decode().rstrip('\n').split('|')
             second_part_bytes = bytes.fromhex(parts[1])
 
@@ -52,4 +99,26 @@ def listen():
                 raise ValueError("Signature must be 64 bytes")
 
             conn.sendall(signature)
+            #currently pyperclip works with only text
+            #if you copy any image or such - it gives you an empty string
+
+            last_clipboard_txt = ""
+            while True : 
+                # now the socket stays open 
+                # track whether you have to send or receive data
+                # in case you receive data - update the clipboard
+                # if the data is changed in clipboard - send to mobile (if text)
+
+                curr_clipboard_txt = pyperclip.paste()
+                if curr_clipboard_txt != last_clipboard_txt and curr_clipboard_txt != "":
+                    last_clipboard_txt = curr_clipboard_txt
+                    data = TEXT_DATA + encode_data(last_clipboard_txt.encode())
+                    conn.sendall(data)
+
+                rlist, _, _ = select.select([server],[],[],5)
+                if rlist:
+                   data = 
+
+                    
+                
 
