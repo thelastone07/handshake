90dfbc5206e4aa778e597c6f273ed0bca21f4174
Dhananjaya Swain <dhananjayaswain99@gmail.com>
2025-07-21 20:19:37 +0530

    thread logic done - untested. works fine till handshake

--- pc_app/network/listen.py
+++ pc_app/network/listen.py
@@ -3,13 +3,14 @@ from network.port_info import LOCAL_PORT, get_public_ip
 from crypto.keygen import sign_challenge
 import pyperclip
 import select
-
+import struct
+import threading  
+import time
 
 NO_OF_CONNECTIONS = 5
 
-TEXT_DATA = b'x\01'
-IMAGE_DATA = b'x\02'
-B_IMAGE_DATA = b'x\03'
+last_clipboard_text = ""
+last_clipboard_text_rec = ""
 
 
 def read(conn, stop=b'\x00', buffer_size = 1024):
@@ -17,18 +18,73 @@ def read(conn, stop=b'\x00', buffer_size = 1024):
     Reads data from the connection until the last byte(s) match STOP.
     """
     data = b''
- 
-    stop_len = len(stop)
     while True:
         chunk = conn.recv(buffer_size)
         if not chunk:
             break
-        data += chunk
-        # Check if the last bytes of data match the stop sequence
-        if data[-stop_len:] == stop:
-            break
+        data = data + chunk
+        while stop in data:
+          packet , data = data.split(stop,1)
+          if not packet:
+             continue
+          yield packet
+
+def read_intial(conn, stop=b'\n', buffer_size = 1024):
+   
+   while True:
+      chunk = conn.recv(buffer_size)
+      if not chunk:
+         break
+      return chunk
+        
+def handle_packet(msg_type:bytes, fmt:bytes, payload:bytes):
+  '''
+  recives decoded payload
+  '''
+  global last_clipboard_text_rec
+  if msg_type == b'T':
+    if fmt == b'txt':
+       pyperclip.copy(payload.decode())
+       last_clipboard_text_rec = payload.decode()
        
-    return data
+  if msg_type == b'I':
+    if fmt == b'png':
+      pass
+    if fmt == b'jpg':
+      pass
+
+def format_msg(data : bytes,msg_type :bytes,fmt :bytes)->bytes:
+   header = struct.pack('!c3sI', msg_type, fmt, len(data))
+   data = header + data
+   return encode_data(data)
+      
+def send_msg(conn):
+   '''
+  currently pyperclip works with only text
+  if you copy any image or such - it gives you an empty string
+  '''
+   while True:
+    global last_clipboard_text_rec
+    global last_clipboard_text
+    curr_clipboard_text = pyperclip.paste()
+    if curr_clipboard_text != last_clipboard_text and curr_clipboard_text != last_clipboard_text_rec and curr_clipboard_text != "":
+        data = format_msg(curr_clipboard_text.encode(),b'T', b'txt')
+        conn.sendall(data)
+        last_clipboard_text = curr_clipboard_text
+    time.sleep(0.5)
+      
+
+def receive_msg(sock,conn):
+   while True:
+      ready, _, _ = select.select([sock], [], [], 1.0)
+      if ready:
+         for data in read(conn):
+          decoded = decode_data(data)
+          msg_type, fmt, size = struct.unpack('!c3sI', decoded[:8])
+          payload = decoded[8:]
+          assert len(payload) == size 
+          handle_packet(msg_type, fmt, payload)
+    
 
 def encode_data(data: bytes) -> bytes:
   encoded = bytearray()
@@ -66,11 +122,6 @@ def decode_data(data:bytes) ->bytes:
     
   return bytes(decoded)
 
-def handle_image(data : bytes):
-   '''
-   helper function to handle image data
-   '''
-
 
 
 
@@ -83,59 +134,34 @@ def listen():
     ip = get_public_ip()
     server.bind((f'{ip}', LOCAL_PORT))
     server.listen(NO_OF_CONNECTIONS)
-    server.setblocking(0)
 
     while True:
-        conn, _ = server.accept()
-        with conn:
-            data = read(conn)
+        ready, _, _ = select.select([server], [], [], 1.0)
+        if ready:
+          conn, _ = server.accept()
+          with conn: 
+              data = read_intial(conn)
+              parts = data.decode().rstrip('\n').split('|')
+              second_part_bytes = bytes.fromhex(parts[1])
+              signature = sign_challenge(second_part_bytes)
+              # Ensure signature is bytes and exactly 64 bytes
+              if not isinstance(signature, bytes):
+                  raise ValueError("Signature must be bytes")
+              if len(signature) != 64:
+                  raise ValueError("Signature must be 64 bytes")
+
+              conn.sendall(signature)
+
+              recv_thread = threading.Thread(target=receive_msg, args=(server,conn), daemon = True)
+              send_thread = threading.Thread(target=send_msg,args=(conn,),daemon=True)
+
+              recv_thread.start()
+              send_thread.start()
+
+              print("threading started")              
             
-            parts = data.decode().rstrip('\n').split('|')
-            second_part_bytes = bytes.fromhex(parts[1])
-
-            signature = sign_challenge(second_part_bytes)
-
-            # Ensure signature is bytes and exactly 64 bytes
-            if not isinstance(signature, bytes):
-                raise ValueError("Signature must be bytes")
-            if len(signature) != 64:
-                raise ValueError("Signature must be 64 bytes")
-
-            conn.sendall(signature)
-            '''
-            currently pyperclip works with only text
-            if you copy any image or such - it gives you an empty string
-            '''
-            last_clipboard_txt = ""
-            last_clipboard_txt_recieved = ""
-            clipboard_state = True
-            while True :
-                ''' 
-                now the socket stays open 
-                track whether you have to send or receive data
-                in case you receive data - update the clipboard
-                if the data is changed in clipboard - send to mobile (if text)
-                '''
-
-                curr_clipboard_txt = pyperclip.paste()
-                if clipboard_state and curr_clipboard_txt != last_clipboard_txt and curr_clipboard_txt != last_clipboard_txt_recieved and curr_clipboard_txt != "":
-                    last_clipboard_txt = curr_clipboard_txt
-                    data = TEXT_DATA + encode_data(last_clipboard_txt.encode())
-                    conn.sendall(data)
-
-                ready, _, _ = select.select([server],[],[],5)
-                if ready:
-                  rec_data= read(conn)
-                  rec_data = decode_data(rec_data)
-
-                  if dtype == "text" and clipboard_state:
-                    last_clipboard_txt_recieved = rec_data
-                    pyperclip.paste()
-                  if dtype == "image":
-                     '''
-                     if image store it in convert it to image and store it in downloads
-                     '''
-                     handle_image(rec_data)
+           
+                
                   
                       
                       
